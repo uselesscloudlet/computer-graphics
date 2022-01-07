e8199d5f978edf3f38b0bfcab6b4efa9e3a7d5a2
Dmitry <54158357+uselesscloudlet@users.noreply.github.com>
2022-01-07 21:46:29 +0300

    lambert shading, meh

--- bumblebee_flight.py
+++ bumblebee_flight.py
@@ -42,6 +42,12 @@ class Point3D():
     def __repr__(self):
         return f'({self.x}, {self.y}, {self.z})'
 
+    def __sub__(self, other):
+        return Point3D(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def to_arr(self):
+        return np.array([self.x, self.y, self.z])
+
     def rotate_X(self, angle):
         sinA, cosA = self.__determ_sincos(angle)
         y = self.y * cosA - self.z * sinA
@@ -60,10 +66,10 @@ class Point3D():
         y = self.x * sinA + self.y * cosA
         return Point3D(x, y, self.z)
 
-    def project(self):
-        factor = FOV / (VIEWER_DISTANCE + self.z)
-        x = self.x * factor + SCREEN_WIDTH / 2
-        y = -self.y * factor + SCREEN_HEIGHT / 2
+    def project(self, x, y, z):
+        factor = FOV / (VIEWER_DISTANCE + self.z + z)
+        x = (self.x + x) * factor + SCREEN_WIDTH / 2
+        y = (-self.y + y) * factor + SCREEN_HEIGHT / 2
         return Point3D(x, y, self.z)
 
     def __determ_sincos(self, angle):
@@ -72,6 +78,12 @@ class Point3D():
         sinA = np.sin(rad)
 
         return sinA, cosA
+
+
+class PointLight(Point3D):
+    def __init__(self, point, color):
+        self.point = point
+        self.color = color
 
 
 class Cube():
@@ -92,11 +104,21 @@ class Cube():
                       (4, 0, 3, 7),
                       (0, 4, 5, 1),
                       (3, 2, 6, 7)]
-        self.colors = [(np.random.randint(0, 255, 3)) for _ in np.arange(6)]
+        # self.colors = [(np.random.randint(0, 255, 3)) for _ in np.arange(6)]
         self.angles = [0, 0, 0]
+        self.pos = [0, 0, 0]
+        self.colors = [0] * 6
 
-    def draw_cube(self):
+    def lambert_make_color(self, point_lights):
+        intensities = self.lambert_lighting(point_lights)
+        #print(intensities)
+        for i in range(len(self.colors)):
+            self.colors[i] = intensities[i] * 255
+        #print(self.colors)
+
+    def draw_cube(self, point_lights):
         t_vertices = self.transform_vertices()
+        self.lambert_make_color(point_lights)
         avg_Z = self.calculate_avg_z(t_vertices)
         polygons = []
         for z_val in sorted(avg_Z, key=lambda x: x[1], reverse=True):
@@ -115,6 +137,11 @@ class Cube():
             polygons.append((point_list, f_index))
         return polygons
 
+    def translate_cube(self, x, y, z):
+        self.pos[0] += x
+        self.pos[1] += y
+        self.pos[2] += z
+
     def rotate_cube(self, direction):
         if direction == Direction.UP:
             self.angles[0] += 2
@@ -124,25 +151,17 @@ class Cube():
             self.angles[1] += 2
         elif direction == Direction.RIGHT:
             self.angles[1] -= 2
-            
-    def translate_cube(self, x, y, z):
-        new_points = []
-        for vertex in self.vertices:
-            p = [vertex.x, vertex.y, vertex.z, 1]
-            t = np.identity(4)
-            t[3][0], t[3][1], t[3][2] = x, y, z
-            d = np.matmul(p, t)
-            new_points.append(Point3D(d[0], d[1], d[2]))
-        # print(self.vertices)
-        # print(new_points)
-        self.vertices = new_points
 
-    def transform_vertices(self):
+    def transform_vertices(self, project=True):
         t_vertices = []
         for vertex in self.vertices:
             rotation = vertex.rotate_X(self.angles[0]).rotate_Y(
                 self.angles[1]).rotate_Z(self.angles[2])
-            projection = rotation.project()
+            if project:
+                projection = rotation.project(
+                    self.pos[0], self.pos[1], self.pos[2])
+            else:
+                projection = rotation
             t_vertices.append(projection)
         # print(f'{self.vertices} -> {t_vertices}')
         return t_vertices
@@ -157,6 +176,47 @@ class Cube():
             avg_z.append([idx, z])
 
         return avg_z
+
+    def lambert_lighting(self, point_lights):
+        intensities = np.zeros((len(self.faces), 3))
+        
+        for light in point_lights:
+            transformed_vertices = self.transform_vertices(False)
+            centres, normals = self.get_c_and_norm(transformed_vertices)
+            light_vectors = [light.point - center for center in centres]
+            for i in np.arange(len(light_vectors)):
+                light_vector = light_vectors[i].to_arr(
+                ) / np.linalg.norm(light_vectors[i].to_arr())
+                normal = normals[i] / np.linalg.norm(normals[i])
+                intensity_coef = np.maximum(np.dot(light_vector, normal), 0)
+                #print(intensity_coef)
+                for j in np.arange(3):
+                    intensities[i][j] += 1 * light.color[j] * intensity_coef
+        return np.array(intensities)
+
+    def get_c_and_norm(self, transformed_vertices):
+        normals = []
+        centres = []
+        for face in self.faces:
+            sum_x = 0
+            sum_y = 0
+            sum_z = 0
+            for vertex in face:
+                point = transformed_vertices[vertex]
+                sum_x += point.x
+                sum_y += point.y
+                sum_z += point.z
+            center = Point3D(sum_x / 4, sum_y / 4, sum_z / 4)
+            p1 = transformed_vertices[face[0]].to_arr()
+            p2 = transformed_vertices[face[1]].to_arr()
+            p3 = transformed_vertices[face[2]].to_arr()
+            v1 = p3 - p1
+            v2 = p2 - p1
+            normal = np.cross(v1, v2)
+
+            centres.append(center)
+            normals.append(normal)
+        return centres, normals
 
 
 class Simulation():
@@ -200,9 +260,9 @@ class Simulation():
             # Sets the texts of each button (counts left to right then top to bottom)
             texts=('1', '2', '3'),
             # When clicked, print number
-            onClicks=(lambda: print('1'), lambda: print('2'), lambda: print('3'))
+            onClicks=(lambda: print('1'), lambda: print(
+                '2'), lambda: print('3'))
         )
-
 
     def create_cube(self):
         if len(self._objects) > 2:
@@ -212,20 +272,21 @@ class Simulation():
 
     def run(self):
         self.add_cube_btn()
+        point_lights = [PointLight(Point3D(5, 5, 0), [1, 1, 0]), PointLight(Point3D(-5, 5, 0), [0, 0, 1])]
         while True:
             self.clock.tick(FPS)
             self.screen.fill(Color.WHITE.value)
-            events=pygame.event.get()
+            events = pygame.event.get()
             for event in events:
                 if event.type == pygame.QUIT:
                     pygame.quit()
-            keys=pygame.key.get_pressed()
+            keys = pygame.key.get_pressed()
             for obj in self._objects:
-                polygons=obj.draw_cube()
+                polygons = obj.draw_cube(point_lights)
                 for polygon in polygons:
-                    figure=polygon[0]
-                    color=polygon[1]
-                    # pygame.draw.polygon(self.screen, obj.colors[color], figure)
+                    figure = polygon[0]
+                    color = polygon[1]
+                    pygame.draw.polygon(self.screen, obj.colors[color], figure)
                     pygame.draw.aalines(self.screen, Color.BLACK.value, True, figure)
                 if keys[pygame.K_UP]:
                     obj.rotate_cube(Direction.UP)
@@ -237,11 +298,13 @@ class Simulation():
                     obj.rotate_cube(Direction.RIGHT)
                 elif keys[pygame.K_F2]:
                     obj.translate_cube(0, 0.05, 0)
+                elif keys[pygame.K_F3]:
+                    obj.lambert_make_color()
 
-            font=pygame.font.Font(None, 26)
-            fps_text=font.render(
+            font = pygame.font.Font(None, 26)
+            fps_text = font.render(
                 f'FPS: {np.round(self.clock.get_fps())}', True, Color.BLACK.value)
-            place=fps_text.get_rect(
+            place = fps_text.get_rect(
                 center=(SCREEN_WIDTH - 50, SCREEN_HEIGHT - 20))
             self.screen.blit(fps_text, place)
 
